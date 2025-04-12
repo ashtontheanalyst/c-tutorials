e1a154c867ef6bbee03028b9f9436fdc3e839746
ashtontheanalyst <ashtonrwsmith.co@gmail.com>
2025-04-12 14:31:39 -0500

    hello world and rupert the dog

---
+++ helloRupert/helloRupert.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // This includes some standard things for our code
+
+// The main function
+int main() {
+    printf("Hello, Rupert!\n"); // Print this to output
+    // \n creates a new like
+
+    printf("\tRupert is a good boy"); // \t is a tab
+
+    return 0; // Returns the exit status, a 0 is success a 1 is an error
+}
---
+++ helloWorld/helloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // This includes some standard things for our code
+
+// The main function
+int main() {
+    printf("Hello, World\n"); // Print this to output
+
+    return 0; // Returns the exit status, a 0 is success a 1 is an error
+}
