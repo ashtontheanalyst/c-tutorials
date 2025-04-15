9487595383cd0d09b30161ab517749b1c095c54c
ashtontheanalyst <ashtonrwsmith.co@gmail.com>
2025-04-15 16:08:05 -0500

    Done with tutorial

--- helloRupert/helloRupert.c
+++ helloRupert/helloRupert.c
@@ -5,7 +5,7 @@ int main() {
     printf("Hello, Rupert!\n"); // Print this to output
     // \n creates a new like
 
-    printf("\tRupert is a good boy"); // \t is a tab
+    printf("\tRupert is a good boy\n"); // \t is a tab
 
     return 0; // Returns the exit status, a 0 is success a 1 is an error
 }
